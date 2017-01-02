e1642853044491ebfe52596603d179d1f62e040a
Shubham Mathur <mathur1995@gmail.com>
2017-01-03 00:30:27 +0530

    formspree API invoked for messages

--- js/contact_me.js
+++ js/contact_me.js
@@ -13,19 +13,17 @@ $(function() {
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
+			var finalmessage = "Name: "+name+"\nEmail id: "+email_address+"\nPhone: "+phone+"\nMessage: "+message
             // Check for white space in name for Success/Fail message
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
+					url: "https://formspree.io/mathur1995@gmail.com", 
+					method: "POST",
+					data: {message: finalmessage},
+					dataType: "json"
+				},
                 cache: false,
                 success: function() {
                     // Success message
